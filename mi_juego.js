6a995c75b8c23d9e5805d977668004b8a8a00656
Cerebrus <dr2208384@gmail.com>
2024-02-27 21:34:58 -0300

    ULTIMOS PASOS

--- mi_juego.js
+++ mi_juego.js
@@ -1,13 +1,3 @@
-/*document.querySelectorAll('.menu button').forEach(button => {
-    button.addEventListener('click', () => {
-        try {
-            console.log(`Has hecho clic en el botón ${button.textContent}`);
-        } catch (error) {
-            console.error('Ocurrió un error al hacer clic en el botón:', error);
-        }
-    });
-});*/
-
 // Seleccionamos los botones
 const botones = document.querySelectorAll('button');
 
@@ -28,17 +18,30 @@ const addAnimation = (button, animationName) => {
     button.classList.add(animationName);
 };
 
-// Primer botón: recorre toda la página con un círculo y vuelve al lugar de inicio
-botones[0].addEventListener('click', () => addAnimation(botones[0], 'circular'));
-
-// Segundo botón: va de arriba a abajo dos veces hasta volver al punto de partida
-botones[1].addEventListener('click', () => addAnimation(botones[1], 'upDown'));
-
-// Tercer botón: cambia en varios colores y muestra un mensaje
-botones[2].addEventListener('click', () => {
-    addAnimation(botones[2], 'colorChange');
-    alert('Aquí se muestra cómo jugar');
+// Asignamos las funciones a los botones
+botones.forEach((boton, index) => {
+    switch (index) {
+        case 0:
+            // Primer botón: recorre toda la página con un círculo y vuelve al lugar de inicio
+            boton.addEventListener('click', () => addAnimation(boton, 'circular'));
+            break;
+        case 1:
+            // Segundo botón: va de arriba a abajo dos veces hasta volver al punto de partida
+            boton.addEventListener('click', () => addAnimation(boton, 'upDown'));
+            break;
+        case 2:
+            // Tercer botón: cambia en varios colores y muestra un mensaje
+            boton.addEventListener('click', () => {
+                addAnimation(boton, 'colorChange');
+                alert('Aquí se muestra cómo jugar');
+            });
+            break;
+        case 3:
+            // Cuarto botón: hace movimientos al azar por la pantalla cada vez que se le da click
+            boton.addEventListener('click', () => moveButtonRandomly(boton));
+            break;
+    }
 });
 
-// Cuarto botón: hace movimientos al azar por la pantalla cada vez que se le da click
-botones[3].addEventListener('click', () => moveButtonRandomly(botones[3]));
+// JavaScript para reproducir el audio automáticamente
+document.getElementById('myAudio').play();
